2b06bff09d74958cae3283488b2039788f1b236f
VIVOBOOK <mg.pereze@uea.edu.ec>
2024-08-29 04:59:36 -0500

    Deber Semana 11

--- Unidad 3.2 Operaciones con arreglos/Ejemplo2.py
+++ Unidad 3.2 Operaciones con arreglos/Ejemplo2.py
@@ -3,3 +3,22 @@ matriz = [[22, 13, 15],
           [9, 25, 11],
           [1, 18, 6]
 ]
+print(matriz)
+
+#funcion de metodo de ordenamiento de buble_sort
+
+def buble_sort(fila):
+    #algoritmo de buble_sort
+    n = len(fila)
+    for i in range(n):
+        for j in range(n-2, 0, -2):
+            if fila [j] > fila [j-1]:
+                fila[j], fila[j-1] = fila[j-1], fila[j]
+                print(fila)
+
+buble_sort (matriz)
+print("matriz original ")
+print(matriz)
+buble_sort(matriz[2])
+print(matriz)
+
